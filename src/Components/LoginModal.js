162fff34d79c32c24dab73094fdae5059ee86584
Hanny <zyhaierciyuan@gmail.com>
2025-02-27 10:12:26 +1100

    Load loyalty when activate

--- src/Components/LoginModal.js
+++ src/Components/LoginModal.js
@@ -174,6 +174,7 @@ const LoginModal = ({ show, onHide }) => {
         "class": memberData.MembershipClass,
         "exp": memberData.Expiry,
         "points": memberData.Points,
+        "loyalty": memberData.Loyalty,
       }), { expires: 7 });
       navigate('/member-center');
       window.location.reload();
