ffa3c03ce041b38e7ed1e2fcb2a398c1c80a4dff
Hanny658 <hannyanhai@gmail.com>
2025-09-18 16:32:09 +1000

    Add Media Icon and links to the footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -31,65 +31,81 @@ const Footer = () => {
 
     return (
         <>
-        <div className="footer-delimiter" />
-        <Container className="footer-overall">
-            <Row>
-                <Col sm={12} md={6} className="mb-4 mb-md-0">
-                    <Row>
-                        <Image src="logo512.png" className="footer-logo" />
-                    </Row>
-                    <Row className="justify-content-start">
-                        <span className="footer-links-row">
-                            <a href="/" className="footer-quick-links footer-texts">首页</a>
-                            <a href="/membership" className="footer-quick-links footer-texts">会员</a>
-                            <a href="/services" className="footer-quick-links footer-texts">服务</a>
-                            <a href="/contact-us" className="footer-quick-links footer-texts">联系我们</a>
-                            <DoTermsAndConditions className="footer-quick-links footer-texts" defaultLang="zh" />
-                        </span>
-                    </Row>
-                </Col>
-                <Col sm={12} md={6} >
-                    <Row>
-                        {/* Text Section */}
-                        <Col xs={12}>
-                            <p className="footer-texts mb-2">
-                                从 1# Club 获取最新资讯! <br />
-                            </p>
-                        </Col>
+            <div className="footer-delimiter" />
+            <Container className="footer-overall">
+                <Row>
+                    <Col sm={12} md={5} className="mb-4 mb-md-0">
+                        <Row>
+                            <Image src="logo512.png" className="footer-logo" />
+                        </Row>
+                        <Row className="justify-content-start">
+                            <span className="footer-links-row">
+                                <a href="/" className="footer-quick-links footer-texts">首页</a>
+                                <a href="/membership" className="footer-quick-links footer-texts">会员</a>
+                                <a href="/services" className="footer-quick-links footer-texts">服务</a>
+                                <a href="/contact-us" className="footer-quick-links footer-texts">联系我们</a>
+                                <DoTermsAndConditions className="footer-quick-links footer-texts" defaultLang="zh" />
+                            </span>
+                        </Row>
+                    </Col>
 
-                        {/* Input and Button Section */}
-                        <Col xs={12} className="d-flex justify-content-end align-items-center">
-                            <Form.Group controlId="formEmail" className="mb-0 me-2">
-                                <Form.Control
-                                    type="email"
-                                    placeholder="请输入您的邮箱
+                    {/* Media icons and links */}
+                    <Col
+                        sm={12}
+                        md={3}
+                        className="d-flex flex-row align-items-center justify-content-center"
+                    >
+                        <a href="https://space.bilibili.com/3546947187116250" target="_blank" className="mx-3 my-md-2">
+                            <Image src="/Icons/bilibili.png" width={32} height={32} />
+                        </a>
+                        <a href="https://www.facebook.com/profile.php?id=61578742085250" target="_blank" className="mx-3 my-md-2">
+                            <Image src="/Icons/facebook.png" width={32} height={32} />
+                        </a>
+                        <a href="https://www.instagram.com/1_club_official" target="_blank" className="mx-3 my-md-2">
+                            <Image src="/Icons/instagram.png" width={32} height={32} />
+                        </a>
+                    </Col>
+                    <Col sm={12} md={4} >
+                        <Row>
+                            {/* Text Section */}
+                            <Col xs={12}>
+                                <p className="footer-texts mb-2">
+                                    从 1# Club 获取最新资讯! <br />
+                                </p>
+                            </Col>
+
+                            {/* Input and Button Section */}
+                            <Col xs={12} className="d-flex justify-content-end align-items-center">
+                                <Form.Group controlId="formEmail" className="mb-0 me-2">
+                                    <Form.Control
+                                        type="email"
+                                        placeholder="请输入您的邮箱
                                     "
-                                    value={email}
-                                    onChange={(e) => setEmail(e.target.value)}
-                                    required
-                                    className="rounded-start"
-                                />
-                            </Form.Group>
-                            <Button
-                                variant="dark"
-                                type="submit"
-                                onClick={subscribMe}
-                                className="rounded-end subscribe-button"
-                            >
-                                订阅
-                            </Button>
-                        </Col>
+                                        value={email}
+                                        onChange={(e) => setEmail(e.target.value)}
+                                        required
+                                        className="rounded-start"
+                                    />
+                                </Form.Group>
+                                <Button
+                                    variant="dark"
+                                    type="submit"
+                                    onClick={subscribMe}
+                                    className="rounded-end subscribe-button"
+                                >
+                                    订阅
+                                </Button>
+                            </Col>
 
-                        {/* Alerts Section */}
-                        <Col xs={12}>
-                            {message && <Alert variant="success" className="mt-2 footer-notification">{message}</Alert>}
-                            {error && <Alert variant="danger" className="mt-2 footer-notification">{error}</Alert>}
-                        </Col>
-                    </Row>
-
-                </Col>
-            </Row>
-        </Container>
+                            {/* Alerts Section */}
+                            <Col xs={12}>
+                                {message && <Alert variant="success" className="mt-2 footer-notification">{message}</Alert>}
+                                {error && <Alert variant="danger" className="mt-2 footer-notification">{error}</Alert>}
+                            </Col>
+                        </Row>
+                    </Col>
+                </Row>
+            </Container>
         </>
     );
 };
