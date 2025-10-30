9df51d29bcb6201cc330e5a89a7d008040a1ac95
Hanny658 <hannyanhai@gmail.com>
2025-10-30 16:51:49 +0800

    Adding another cheaper selection for testing
    Test payment succeed

--- src/Views/StripeTest.jsx
+++ src/Views/StripeTest.jsx
@@ -2,7 +2,8 @@ import StripeCheckoutButton from "../Components/StripeCheckoutButton";
 
 function StripeTest() {
     return (
-        <><div className="sticky-navbar-page-start-placeholder" />
+        <>
+            <div className="sticky-navbar-page-start-placeholder" />
             <div className="card text-center p-3">
                 <img
                     src="https://i.imgur.com/EHyR2nP.png"
@@ -17,6 +18,23 @@ function StripeTest() {
                         priceId="price_1SNlkw748iCYpCCmDYaBX0Yj"
                         amount="$20.00"
                         title="Not Yet a Product"
+                    />
+                </div>
+            </div>
+            <div className="card text-center p-3">
+                <img
+                    src="logo512.png"
+                    alt="Book Cover"
+                    className="card-img-top mx-auto"
+                    style={{ width: "150px" }}
+                />
+                <div className="card-body">
+                    <h5 className="card-title">Nah Once the Test</h5>
+                    <p className="card-text">$5.00</p>
+                    <StripeCheckoutButton
+                        priceId="price_1SNqf0748iCYpCCmOXu9HK7Q"
+                        amount="$5.00"
+                        title="Nah Once the Test"
                     />
                 </div>
             </div>
